0a4dbb133e527a6d4f508b70dd740d5c241c9e1b
Roman Popat <romanpopat@gmx.com>
2017-06-01 19:06:39 +0100

    fixed license and docs

--- newsR/R/newsapi_sources.R
+++ newsR/R/newsapi_sources.R
@@ -1,14 +1,21 @@
 #' newsapiSources
 #'
-#' @param
+#' @param language (optional) - The 2-letter ISO-639-1 code of the language you would like to get sources for.
+#'                 Possible options: "en", "de", "fr." Default: empty (all sources returned)
+#' @param country (optional) - The 2-letter ISO 3166-1 code of the country you would like to get sources for.
+#'                Possible options: "au", "de", "gb", "in", "it", "us". Default: empty (all sources returned)
+#' @param category (optional) - The category you would like to get sources for.
+#'                 Possible options: business, entertainment, gaming, general, music, politics, science-and-nature, sport, technology.
+#'                 Default: empty (all sources returned)
 #'
 #' @keywords SIMD, openSIMD, simdr
 #'
+#' @import jsonlite
 #' @export
 newsapiSources <- function(
-  language = "en",
-  category = "",
-  country = "gb"
+  language = "",
+  country = "",
+  category = ""
 ) {
 
   this_url <- paste0("https://newsapi.org/v1/sources?",
