b22b834a68cee67c756f6b63f0f690ea0c9e733c
rmnppt <romanpopat@gmx.com>
2017-06-02 15:52:42 +0000

    draft of getting article data from newsapi.org

--- newsapi_databot/scripts/collect_sources.R
+++ newsapi_databot/scripts/collect_sources.R
@@ -1,5 +1,5 @@
-date()
-
+if(!require(RMySQL)) install.packages("RMySQL")
+if(!require(tidyverse)) install.packages("tidyverse")
 if(!require(devtools)) install.packages("devtools")
 if(!require(newsR)) {
   devtools::install_github("rmnppt/newsfuzz/newsR")
@@ -9,4 +9,27 @@ if(!require(newsR)) {
 sources <- newsapiSources("en", "gb")
 saveRDS(sources, "data/sources.rds")
 
-articles <- lapply(sources$sources$id, function(s) newsapiArticles(source = s))
+articles <- sources$sources$id %>%
+  lapply(function(s) newsapiArticles(source = s)) %>%
+  lapply(function(s) jsonlite::flatten(as.data.frame(s))) %>%
+  do.call(rbind, .)
+
+names(articles) <- sub("articles.", "", names(articles))
+
+timelast <- readRDS("data/lastdownloaded.rds")
+if(exists(timelast)) {
+  articles <- articles %>%
+    filter(publishedAt > timelast)
+}
+
+con <- dbConnect(RMySQL::MySQL(), 
+                 host = "", 
+                 port = 3306,
+                 dbname = "newsfuzz",
+                 user = , 
+                 password = )
+dbWriteTable(con, "articles", d_small, append = TRUE)
+dbDisconnect(con)
+
+timenow <- print(Sys.time())
+saveRDS(timenow, "data/lastdownload.rds")
