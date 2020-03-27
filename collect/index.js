1a77b938b57a39d63d22629993c4a1e1a20ee238
Roman Popat <roman@datakitchen.co.uk>
2020-03-27 13:29:23 +0000

    setting up to collect articles

--- collect/index.js
+++ collect/index.js
@@ -1,6 +1,7 @@
 const NewsAPI = require('newsapi');
 const { Firestore } = require('@google-cloud/firestore');
 const { Sources } = require('./entities');
+require('dotenv').config();
 
 const newsapi = new NewsAPI(process.env.NEWSAPIKEY);
 
@@ -14,13 +15,29 @@ function getSources(language = 'en') {
   return sources;
 }
 
+function getArticles(sources, from, to, language = 'en') {
+  const articles = newsapi.v2.everything({
+    sources,
+    from,
+    to,
+    language,
+    pageSize: 100,
+    page: 1,
+  });
+  // TODO - need to add paging here for the prolific sources
+  return articles;
+}
+
 exports.updateSourcesCollection = function updateSourcesCollection() {
   getSources()
     .catch('Failed to get the sources from newsapi.org.')
     .then((response) => {
-      console.log(`status: ${response.status}`);
+      console.log(`response status: ${response.status}`);
       console.log(`number of sources: ${response.sources.length}`);
 
+      // Roman 27/03/20
+      // Im putting them into an explicit data model, #
+      // will possibly bundle some QC in there.
       const sourceObj = new Sources(response.sources);
       const sources = sourceObj.getAll();
 
@@ -29,4 +46,14 @@ exports.updateSourcesCollection = function updateSourcesCollection() {
         doc.set(source, { merge: true });
       });
     });
-}
+};
+
+exports.updateArticlesCollection = function updateArticlesCollection() {
+  sources_collection.get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, ' => ', doc.data());
+      });
+    });
+};
--- collect/tests/test_api.js
+++ collect/tests/test_api.js
@@ -1,54 +1,5 @@
-const NewsAPI = require('newsapi');
+require('dotenv').config('../');
+const { updateSourcesCollection, updateArticlesCollection } = require('../index');
 
-const auth = require('./auth');
-const newsapi = new NewsAPI(auth.key);
-
-// To query /v2/top-headlines
-// All options passed to topHeadlines are optional, but you need to include at least one of them
-// newsapi.v2.topHeadlines({
-//   sources: 'bbc-news,the-verge',
-//   q: 'bitcoin',
-//   category: 'business',
-//   language: 'en',
-//   country: 'us'
-// }).then(response => {
-//   console.log(response);
-//   /*
-//     {
-//       status: "ok",
-//       articles: [...]
-//     }
-//   */
-// });
-
-// // To query /v2/everything
-// // You must include at least one q, source, or domain
-// newsapi.v2.everything({
-//   q: 'bitcoin',
-//   sources: 'bbc-news,the-verge',
-//   domains: 'bbc.co.uk, techcrunch.com',
-//   from: '2017-12-01',
-//   to: '2017-12-12',
-//   language: 'en',
-//   sortBy: 'relevancy',
-//   page: 2
-// }).then(response => {
-//   console.log(response);
-//   /*
-//     {
-//       status: "ok",
-//       articles: [...]
-//     }
-//   */
-// });
-
-// To query sources
-// All options are optional
-newsapi.v2.sources({
-  // category: 'technology',
-  language: 'en',
-  // country: 'uk'
-}).then(response => {
-  console.log(`status: ${response.status}`);
-  console.log(`number of sources: ${response.sources.length}`)
-});
+updateSourcesCollection();
+updateArticlesCollection();
