2e9a754c50023aab69e93a2357a01ff442362e3b
Roman Popat <roman@datakitchen.co.uk>
2020-03-27 10:06:04 +0000

    trying to get rid of modules junk

--- collect/index.js
+++ collect/index.js
@@ -1,34 +1,33 @@
 const NewsAPI = require('newsapi');
 const { Firestore } = require('@google-cloud/firestore');
 const { Sources } = require('./entities');
-
 const newsapi_auth = require('./newsapi_auth');
+
 const newsapi = new NewsAPI(newsapi_auth.key);
 
 const firestore = new Firestore();
 const sources_collection = firestore.collection('sources');
 
 function getSources(language = 'en') {
-  sources = newsapi.v2.sources({
-    language: language,
-  })
+  const sources = newsapi.v2.sources({
+    language,
+  });
   return sources;
 }
 
 function updateSourcesCollection() {
   getSources()
-  .catch("Failed to get the sources from newsapi.org.")
-  .then((response) => {
+    .catch('Failed to get the sources from newsapi.org.')
+    .then((response) => {
+      console.log(`status: ${response.status}`);
+      console.log(`number of sources: ${response.sources.length}`);
 
-    console.log(`status: ${response.status}`);
-    console.log(`number of sources: ${response.sources.length}`);
+      const sourceObj = new Sources(response.sources);
+      const sources = sourceObj.getAll();
 
-    sourceObj = new Sources(response.sources);
-    sources = sourceObj.getAll();
-
-    sources.forEach((source) => {
-      const doc = sources_collection.doc(source.id);
-      var setWithMerge = doc.set(source, { merge: true });
+      sources.forEach((source) => {
+        const doc = sources_collection.doc(source.id);
+        doc.set(source, { merge: true });
+      });
     });
-  });
 }
