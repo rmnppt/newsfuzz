0b0ed6a2c6797143c0ce2bf643fac0080d6ef75f
Roman Popat <roman@datakitchen.co.uk>
2020-04-02 14:29:08 +0100

    putting firestore data into dataframe

--- analyse/utils/database.py
+++ analyse/utils/database.py
@@ -1,25 +1,30 @@
 import sys
 from getopt import getopt
 import logging
-import datetime
+from datetime import datetime, timedelta
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import pandas as pd
 
 logging.getLogger().setLevel(logging.INFO)
 
-# Use a service account
-cred = credentials.Certificate('../gcp_auth.json')
-firebase_admin.initialize_app(cred)
-
-db = firestore.client()
-articles_ref = db.collection(u'articles')
 
 def run():
-    last_month = datetime.today() - datetime.timedelta(days=30)
-    logging.info(last_month)
-    query_ref = articles_ref.where(u'publishedAT', u'>', last_month)
+    # Use a service account
+    cred = credentials.Certificate('./gcp_auth.json')
+    firebase_admin.initialize_app(cred)
 
+    db = firestore.client()
+    articles_ref = db.collection(u'articles')
+
+    # analyse articles no more than one month old
+    last_month = datetime.today() - timedelta(days=30)
+    docs = articles_ref.where('publishedAt', '>', last_month).stream()
+
+    articles_list = [i.to_dict() for i in docs]
+    articles = pd.DataFrame(articles_list)
+    logging.info(list(articles))
 
 def main(arguments):
     level = logging.INFO
