7b89c87f03aca5ad8e218d146385f0d370226d69
Roman Popat <roman@datakitchen.co.uk>
2020-04-07 15:25:48 +0100

    model in a class of its own ;-)

--- analyse/analysis/topics.py
+++ analyse/analysis/topics.py
@@ -6,92 +6,118 @@ from utils.database import DocumentDB
 from datetime import datetime, timedelta
 import re
 import pandas as pd
-import jsonpickle
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import NMF
 from afinn import Afinn
 
 logging.getLogger().setLevel(logging.INFO)
 
-class Object:
-    def toJSON(self):
-        return json.dumps(self, default=lambda o: o.__dict__,
-            sort_keys=True, indent=4)
+from google.cloud import storage
 
-def print_top_words(model, feature_names, n_top_words):
-    topic_descriptions = []
-    for topic_idx, topic in enumerate(model.components_):
-        message = "Topic #%d: " % topic_idx
-        top_terms = ", ".join([feature_names[i]
-                               for i in topic.argsort()[:-n_top_words - 1:-1]])
-        message += top_terms
-        logging.info(message)
-        topic_descriptions.append(top_terms)
-    logging.info('')
-    return topic_descriptions
+def upload_blob(bucket_name, data_string, destination_blob_name):
+    """Uploads a file to the bucket."""
+    # bucket_name = "your-bucket-name"
+    # source_file_name = "local/path/to/file"
+    # destination_blob_name = "storage-object-name"
 
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
 
-def getSentiment(text_series):
-    afinn = Afinn()
-    sentiment = [afinn.score(a) for a in text_series]
-    return sentiment
+    blob.upload_from_string(data_string)
 
-
-def getTopics(ids, text_series):
-    n_samples = len(text_series)
-    n_features = 1000
-    n_components = 20
-    n_top_words = 10
-
-    # Use tf-idf features for NMF.
-    logging.info("Extracting tf-idf features for NMF...")
-    tfidf_vectorizer = TfidfVectorizer(
-        max_df=0.8,
-        min_df=2,
-        max_features=n_features,
-        stop_words='english'
+    print(
+        "String uploaded to {}.".format(
+             destination_blob_name
+        )
     )
 
-    t0 = time()
-    tfidf = tfidf_vectorizer.fit_transform(text_series)
+class Model:
+    def __init__(self):
+        self.n_features = 1000
+        self.n_components = 20
+        self.n_top_words = 10
 
-    logging.info("done in %0.3fs." % (time() - t0))
+        self.tfidf_vectorizer = TfidfVectorizer(
+            max_df=0.8,
+            min_df=2,
+            max_features=self.n_features,
+            stop_words='english'
+        )
 
-    # non-negative matrix factorisation
-    # Fit the NMF model
-    logging.info("Fitting the NMF model (Frobenius norm) with tf-idf features, "
-                 "n_samples=%d and n_features=%d..."
-                 % (n_samples, n_features))
-    t0 = time()
-    nmf = NMF(
-        n_components=n_components,
-        random_state=1,
-        alpha=.1,
-        l1_ratio=.5,
-        init="nndsvd"
-    ).fit(tfidf)
-    logging.info("done in %0.3fs." % (time() - t0))
+        self.topic_model = NMF(
+            n_components=self.n_components,
+            random_state=1,
+            alpha=.1,
+            l1_ratio=.5,
+            init="nndsvd"
+        )
 
-    logging.info("Topics in NMF model (Frobenius norm):")
-    tfidf_feature_names = tfidf_vectorizer.get_feature_names()
+    def fit(self, X):
+        self.n_samples = len(X)
+        logging.info("Extracting tf-idf features for NMF...")
+        t0 = time()
+        tfidf = self.tfidf_vectorizer.fit_transform(X)
+        logging.info("done in %0.3fs." % (time() - t0))
 
-    topic_descriptions = print_top_words(nmf, tfidf_feature_names, n_top_words)
+        # Fit the NMF model
+        logging.info("Fitting the NMF model (Frobenius norm) with tf-idf features, "
+                     "n_samples=%d and n_features=%d..."
+                     % (self.n_samples, self.n_features))
+        t0 = time()
+        self.topic_model.fit(tfidf)
+        logging.info("done in %0.3fs." % (time() - t0))
 
-    W = nmf.fit_transform(tfidf)
-    H = nmf.components_
+        self.X = X
+        self.tfidf = tfidf
+        return self
 
-    logging.info(f'Shape of document topic matrix: {W.shape}')
-    logging.info(f'Shape of topic term matrix: {H.shape}')
+    def transform(self):
+        self.article_topics = self.topic_model.fit_transform(self.tfidf)
+        self.sentiment_scores = self.getSentiment(self.X)
+        return self
 
-    model_results = {
-        "document_ids": ids,
-        "terms": tfidf_feature_names,
-        "article_topics": W,
-        "topic_terms": H,
-        "topic_descriptions": topic_descriptions,
-    }
+    def fit_transform(self, X):
+        self.fit(X).transform()
+        return self
 
-    return model_results
+    def print_top_words(self, feature_names, n_top_words):
+        logging.info("Topics in NMF model (Frobenius norm):")
+        topic_descriptions = []
+        for topic_idx, topic in enumerate(self.topic_model.components_):
+            message = "Topic #%d: " % topic_idx
+            top_terms = ", ".join([feature_names[i]
+                                   for i in topic.argsort()[:-n_top_words - 1:-1]])
+            message += top_terms
+            logging.info(message)
+            topic_descriptions.append(top_terms)
+        logging.info('')
+        self.topic_descriptions = topic_descriptions
+
+    def getSentiment(self, text_series):
+        afinn = Afinn()
+        sentiment = [afinn.score(a) for a in text_series]
+        return sentiment
+
+    def results(self):
+        tfidf_feature_names = self.tfidf_vectorizer.get_feature_names()
+        topic_descriptions = self.print_top_words(tfidf_feature_names, self.n_top_words)
+
+        W = self.article_topics
+        H = self.topic_model.components_
+
+        logging.info(f'Shape of document topic matrix: {W.shape}')
+        logging.info(f'Shape of topic term matrix: {H.shape}')
+
+        model_results = {
+            "terms": tfidf_feature_names,
+            "article_topics": W,
+            "topic_terms": H,
+            "topic_descriptions": topic_descriptions,
+            "sentiment_scores": self.sentiment_scores,
+        }
+
+        return model_results
 
 
 def run():
@@ -102,6 +128,7 @@ def run():
         .query('publishedAt', '>', last_month) \
         .toDf()
 
+    # any articles missing content?
     articles = articles.dropna(subset=['content'])
 
     # remove the truncation text
@@ -110,16 +137,19 @@ def run():
     )
 
     # do the analysis
-    topics = getTopics(articles.hash, articles.content)
-    sentiment = getSentiment(articles.content)
+    analysis = Model() \
+        .fit_transform(articles.content) \
+        .results()
 
-    # write it back to the db
-    analysis_results = db.collection('analysis')
+    analysis['article_hashes'] = articles.hash
 
-    # TODO figure out all of the serialisation problems for writing back to the document db 
+    # sense check: is the analysis the right shape?
+    right_shape = len(analysis['article_hashes']) == len(analysis['article_topics']) == len(analysis['sentiment_scores'])
+    if (not right_shape):
+        raise ValueError('Analysis results are the wrong shape')
 
-    # analysis_results.document('topics').write(json.dump(topics))
-    # analysis_results.document('sentiment').write(json.dumps(sentiment))
+    # write it back to the db - this time a cloud storage object
+    upload_blob('newsfuzz-analysis', json.dumps(analysis), 'daily.json')
 
 
 def main(arguments):
