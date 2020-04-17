cb62fe4c2f28821fa158966ad2c2c06641014a64
Roman Popat <roman@datakitchen.co.uk>
2020-04-17 12:54:00 +0100

    something still wrong with the python/docker/gcloud combo

--- analyse/app.py
+++ analyse/app.py
@@ -1,17 +1,53 @@
 import os
+import sys
+from getopt import getopt
+import logging
+import asyncio
 from analysis.run import run
 from flask import Flask
 
+logging.getLogger().setLevel(logging.INFO)
+
 app = Flask(__name__)
 
-@app.route('/')
-def analyse():
+async def async_analysis():
     run()
     return 1
 
+@app.route('/')
+def analyse():
+    asyncio.run(async_analysis())
+    return "analysis started"
+
+# if __name__ == "__main__":
+#     asyncio.run(
+#         app.run(
+#             debug=True,
+#             host='0.0.0.0',
+#             port=int(os.environ.get('PORT', 8080))
+#         )
+#     )
+
+
+def main(arguments):
+    level = logging.INFO
+    options, arguments = getopt(arguments, "l:")
+    for option, argument in options:
+        if option == "-l":
+            level = getattr(logging, argument.upper())
+
+    logging.basicConfig(
+        level=level, format="%(asctime)s:%(levelname)s: %(message)s")
+    try:
+        app.run(
+            debug=True,
+            host='0.0.0.0',
+            port=int(os.environ.get('PORT', 8080))
+        )
+    except Exception:
+        logging.exception('top level catch')
+        return 1
+
+
 if __name__ == "__main__":
-    app.run(
-        debug=True,
-        host='0.0.0.0',
-        port=int(os.environ.get('PORT', 8080))
-    )
+    sys.exit(main(sys.argv[1:]))
